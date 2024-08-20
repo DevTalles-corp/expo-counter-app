bfba286489c9db876d0832194a9f84bed8c23cfc
Fernando Herrera <fernando.herrera85@gmail.com>
2024-08-20 12:03:40 -0400

    chore: Add FAB component for floating action button

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import FAB from './components/FAB';
 
 export default function App() {
+  const [count, setCount] = useState(10);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textHuge}>{count}</Text>
+
+      {/* <Pressable
+        style={styles.floatingButton}
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => setCount(0)}
+      >
+        <Text style={{ color: 'white', fontSize: 20 }}>+1</Text>
+      </Pressable> */}
+
+      <FAB
+        label="+1"
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => setCount(0)}
+        position="right"
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +36,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  textHuge: {
+    fontSize: 120,
+    fontWeight: '100',
   },
 });
---
+++ components/FAB.tsx
@@ -0,0 +1,54 @@
+import { Text, Pressable, StyleSheet } from 'react-native';
+
+interface Props {
+  label: string;
+
+  position?: 'left' | 'right';
+
+  // Methods
+  onPress?: () => void;
+  onLongPress?: () => void;
+}
+
+export default function FAB({
+  label,
+  onPress,
+  onLongPress,
+  position = 'right',
+}: Props) {
+  return (
+    <Pressable
+      style={[
+        styles.floatingButton,
+        position === 'right' ? styles.positionRight : styles.positionLeft,
+      ]}
+      onPress={onPress}
+      onLongPress={onLongPress}
+    >
+      <Text style={{ color: 'white', fontSize: 20 }}>{label}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  floatingButton: {
+    position: 'absolute',
+    bottom: 20,
+
+    backgroundColor: '#65558F',
+    padding: 20,
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    elevation: 3,
+    shadowRadius: 4,
+  },
+
+  positionRight: {
+    right: 20,
+  },
+  positionLeft: {
+    left: 20,
+  },
+});
